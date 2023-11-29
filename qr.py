5033b7bf7b0ac58b7fa0397861feabae538193bf
Bavis0803 <121236266+Bavis0803@users.noreply.github.com>
2023-11-29 21:37:20 +0700

    Delete qr.py

--- qr.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-from pyzbar.pyzbar import decode
-def qr(img_path):
-    img = cv2.imread(img_path)
-    for code in decode(img):
-        decode_data = code.data.decode("utf-8")
-        return decode_data
-print(qr("5.jpg"))
